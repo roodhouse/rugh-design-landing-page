198cc1aefdc8468bea000e041db0b081546a67f5
roodhouse <rughjm@gmail.com>
2023-04-08 20:40:17 -0500

    navbar updates

--- landing-page/src/components/Navbar.jsx
+++ landing-page/src/components/Navbar.jsx
@@ -16,76 +16,81 @@ function Navbar() {
         {/* menu */}
         <ul className='hidden md:flex'>
             <li>Home</li>
-            <li>Color</li>
-            <li>Color Schemes</li>
+            <li className="relative" data-te-dropdown-ref>
+                <a
+                    className="flex items-center whitespace-nowrap motion-reduce:transition-none"
+                    href="#"
+                    type="button"
+                    id="dropdownMenuButton2"
+                    data-te-dropdown-toggle-ref
+                    aria-expanded="false"
+                    data-te-ripple-init
+                    data-te-ripple-color="light">
+                    Color
+                        <span className="ml-2 w-2">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                className="h-5 w-5">
+                                <path
+                                fill-rule="evenodd"
+                                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                </a>
+                <ul
+                    className="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg bg-[#676766] [&[data-te-dropdown-show]]:block"
+                    aria-labelledby="dropdownMenuButton2"
+                    data-te-dropdown-menu-ref>
+                    <li>
+                        <a
+                            className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+                            href="#"
+                            data-te-dropdown-item-ref>
+                            Color Consultation
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+                            href="#"
+                            data-te-dropdown-item-ref>
+                            Color Schemes
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+                            href="#"
+                            data-te-dropdown-item-ref>
+                            Color Review
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+                            href="#"
+                            data-te-dropdown-item-ref>
+                            Color Samples
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            {/* <li>Color Schemes</li>
             <li>Color Review</li>
-            <li>Paint Samples</li>
+            <li>Paint Samples</li> */}
             <li>Portfolio</li>
         </ul>
 
-        {/* copied code  setup tailwind elements first then see if this works*/}
-        <div className="relative" data-te-dropdown-ref>
-  <a
-    className="flex items-center whitespace-nowrap rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] motion-reduce:transition-none"
-    href="#"
-    type="button"
-    id="dropdownMenuButton2"
-    data-te-dropdown-toggle-ref
-    aria-expanded="false"
-    data-te-ripple-init
-    data-te-ripple-color="light">
-    Dropdown link
-    <span className="ml-2 w-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fill-rule="evenodd"
-          d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-          clip-rule="evenodd" />
-      </svg>
-    </span>
-  </a>
-  <ul
-    className="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
-    aria-labelledby="dropdownMenuButton2"
-    data-te-dropdown-menu-ref>
-    <li>
-      <a
-        className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-        href="#"
-        data-te-dropdown-item-ref
-        >Action</a
-      >
-    </li>
-    <li>
-      <a
-        className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-        href="#"
-        data-te-dropdown-item-ref
-        >Another action</a
-      >
-    </li>
-    <li>
-      <a
-        className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-        href="#"
-        data-te-dropdown-item-ref
-        >Something else here</a
-      >
-    </li>
-  </ul>
-</div>
-
         {/* hamburger */}
-        <div onClick={handleClick} className='md:hidden z-10'>
+        <div onClick={handleClick} className={!nav ? 'md:hidden z-10 text-[#676766]' : 'md:hidden z-10 text-white'}>
             {!nav ? <FaBars /> : <FaTimes />}
         </div>
 
         {/* mobile menu */}
-        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center'}>
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center bg-[#676766] text-white'}>
             <li className='py-6 text-4xl'>Home</li>
             <li className='py-6 text-4xl'>Color Consultation</li>
             <li className='py-6 text-4xl'>Color Schemes</li>
