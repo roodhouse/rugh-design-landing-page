fd963f3087fd13d635ad658f85c9fd40c389ef42
roodhouse <rughjm@gmail.com>
2023-04-10 12:56:37 -0500

    image size changes, link changes

--- landing-page/src/components/Consult.jsx
+++ landing-page/src/components/Consult.jsx
@@ -8,10 +8,10 @@ function Consult() {
             <div className='max-w-[100%] sm:max-w-[50%] text-center sm:text-left flex flex-col items-center sm:items-start'>
                 <p className='text-4xl text-[#676766] font-bold inline border-b-4 border-[#E5C1C1]'>Color Consultation</p>
                 <p className='py-4'>Hire me to be your color consultant! By assessing the current undertones in your home, I can find the color or color palette that will make the biggest impact on your space.</p>
-                <a className='text-[#676766] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white' href='https://www.rughdesign.com/product/interior-color-consultation/' target='_blank'>Hire me!</a>
+                <a className='text-[#676766] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white' href='https://www.rughdesign.com/product/interior-color-consultation/'>Hire me!</a>
             </div>
             <div className='max-w-[100%] sm:max-w-[50%] order-first sm:order-last pb-4 sm:pb-0'>
-                <img className='mx-[0%] sm:mx-[25%] max-w-[100%] sm:max-w-[50%]' src={Interior} alt="Interior Color Consultation" />
+                <img className='mx-[0%] sm:mx-[25%] max-w-[100%] sm:max-w-[50%] md:max-w-[75%]' src={Interior} alt="Interior Color Consultation" />
             </div>
         </div>
     </div>
--- landing-page/src/components/Footer.jsx
+++ landing-page/src/components/Footer.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import {FaFacebook, FaInstagram, FaPinterest} from 'react-icons/fa'
+import {FaFacebook, FaInstagram, FaPinterest, FaYoutube} from 'react-icons/fa'
 import { CiMail } from "react-icons/ci";
 
 function Footer() {
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-full pt-60 sm:pt-0'>
         <div className='block items-center bg-[#676766] w-full h-full'>
         <div className='pt-9 flex justify-center'>
             <div className='mb-9 w-[50%] flex justify-evenly'>
-                <a href='#' className=''><FaInstagram size={30} fill={"white"}/></a>
-                <a href='#' className=''><FaFacebook size={30} fill={"white"}/></a>
-                <a href='#' className=''><FaPinterest size={30} fill={"white"}/></a>
-                <a href='#' className=''><CiMail size={30} fill={"white"}/></a>
+                <a href='https://www.instagram.com/rugh.design/' target='_blank'><FaInstagram size={30} fill={"white"}/></a>
+                <a href='https://www.facebook.com/lauraerugh/' target='_blank'><FaFacebook size={30} fill={"white"}/></a>
+                <a href='https://www.pinterest.com/lauraerugh/' target='_blank'><FaPinterest size={30} fill={"white"}/></a>
+                <a href='https://www.youtube.com/channel/UC8k4gmYFHznUWCgvqHMbupA' target='_blank'><FaYoutube size={30} fill={"white"}/></a>
+                <a href='mailto: laura@rughdesign.com' target='_blank'><CiMail size={30} fill={"white"}/></a>
             </div>
         </div>
         <div className='bg-neutral-300 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200'>
-        © 2023 Copyright: Rugh Design | Design by <a className='text-neutral-800 dark:text-white' href='https://roodhouse.github.io/'>roodhouse</a>
+        © 2023 Copyright: Rugh Design | Design by <a className='text-neutral-800 dark:text-white' href='https://roodhouse.github.io/' target='_blank'>roodhouse</a>
         </div>
         </div>
     </div>
@@ -23,34 +24,3 @@ function Footer() {
 }
 
 export default Footer
-
-
-// <!--Footer container-->
-// <footer class="bg-neutral-200 text-center text-white dark:bg-neutral-600">
-//   <div class="container pt-9">
-//     <div class="mb-9 flex justify-center">
-//       <a href="#!" class="mr-9 text-neutral-800 dark:text-neutral-200">
-//         <svg
-//           xmlns="http://www.w3.org/2000/svg"
-//           class="h-4 w-4"
-//           fill="currentColor"
-//           viewBox="0 0 24 24">
-//           <path
-//             d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-//         </svg>
-//       </a>
-
-//     </div>
-//   </div>
-
-//   <!--Copyright section-->
-//   <div
-//     class="bg-neutral-300 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
-//     © 2023 Copyright:
-//     <a
-//       class="text-neutral-800 dark:text-neutral-400"
-//       href="https://tailwind-elements.com/"
-//       >Tailwind Elements</a
-//     >
-//   </div>
-// </footer>
