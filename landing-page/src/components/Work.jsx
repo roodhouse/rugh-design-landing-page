594b44a2a70fc985a53c421cba789bbfb4f464de
roodhouse <rughjm@gmail.com>
2023-04-12 08:01:52 -0500

    fixed img height and width

--- landing-page/src/components/Work.jsx
+++ landing-page/src/components/Work.jsx
@@ -16,12 +16,12 @@ function Work() {
                 <p className='text-4xl text-[#676766] font-bold inline border-b-4 border-[#E5C1C1] pt-4 sm:pt-0'>Portfolio</p>
                 <p className='py-4'>Check out some of my work.</p>
                 <div className='flex flex-col sm:flex-row flex-wrap gap-1 items-center sm:justify-center pb-4'>
-                    <img className='max-w-[100%] sm:max-w-[25%] h-fit border-2 border-[#E5C1C1]' src={PortOne} alt="Agreeable-Gray SW 7029 with Cityscape SW 7067 and Extra White SW 7006" />
-                    <img className='max-w-[100%] sm:max-w-[25%] h-fit border-2 border-[#E5C1C1]' src={PortTwo} alt="Sherwin-Williams Balanced Beige SW 7037 in open space" />
-                    <img className='max-w-[100%] sm:max-w-[25%] h-fit border-2 border-[#E5C1C1]' src={PortThree} alt="Open layout kitchen with Amazing Gray SW 7044 cabinets and White Duck SW 7010 walls" />
-                    <img className='max-w-[100%] sm:max-w-[25%] h-fit border-2 border-[#E5C1C1]' src={PortFour} alt="Balanced Beige Kitchen walls with Creamy Cabinets" />
-                    <img className='max-w-[100%] sm:max-w-[25%] h-fit border-2 border-[#E5C1C1]' src={PortFive} alt="Shoji White on exterior" />
-                    <img className='max-w-[100%] sm:max-w-[25%] h-fit border-2 border-[#E5C1C1]' src={PortSix} alt="Front porch" />
+                    <img className='max-w-[75%] sm:max-w-[25%] border-2 border-[#E5C1C1]' src={PortOne} alt="Agreeable-Gray SW 7029 with Cityscape SW 7067 and Extra White SW 7006" />
+                    <img className='max-w-[75%] sm:max-w-[25%] border-2 border-[#E5C1C1]' src={PortTwo} alt="Sherwin-Williams Balanced Beige SW 7037 in open space" />
+                    <img className='max-w-[75%] sm:max-w-[25%] border-2 border-[#E5C1C1]' src={PortThree} alt="Open layout kitchen with Amazing Gray SW 7044 cabinets and White Duck SW 7010 walls" />
+                    <img className='max-w-[75%] sm:max-w-[25%] border-2 border-[#E5C1C1]' src={PortFour} alt="Balanced Beige Kitchen walls with Creamy Cabinets" />
+                    <img className='max-w-[75%] sm:max-w-[25%] border-2 border-[#E5C1C1]' src={PortFive} alt="Shoji White on exterior" />
+                    <img className='max-w-[75%] sm:max-w-[25%] border-2 border-[#E5C1C1]' src={PortSix} alt="Front porch" />
                 </div>
                 <div className='flex flex-row justify-center w-full'>
                 <a className='text-[#676766] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#E5C1C1] hover:border-[#E5C1C1] hover:text-white' href='/portfolio'>View More</a>
