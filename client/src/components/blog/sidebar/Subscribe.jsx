f316daf44969c924d3579e779866a6cc3e0e515e
roodhouse <rughjm@gmail.com>
2023-05-12 21:22:15 -0500

    added form vaildation via react-hook-form

--- client/src/components/blog/sidebar/Subscribe.jsx
+++ client/src/components/blog/sidebar/Subscribe.jsx
@@ -1,32 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useForm } from 'react-hook-form'
 
 function Subscribe() {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState('')
+    const { register, handleSubmit, setValue, formState: {errors} } = useForm({defaultValues: {
+        firstName: '',
+        lastName: '',
+        email: ''
+    }});
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        console.log(firstName, lastName, email)
-        const newSub = {
-            firstName: firstName,
-            lastName: lastName,
-            email: email 
-        }
-        await fetch('http://localhost:5001/record/sub', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newSub)
-        })
-        .catch(error => {
-            window.alert(error);
-            return;
-        })
-        setFirstName('')
-        setLastName('')
-        setEmail('')
+    const onError = () => {
+        console.log('wrong')
     }
     
   return (
@@ -37,38 +20,55 @@ function Subscribe() {
         <div id='subscribeTextDiv' className='text-center py-4'>
             <p>subscribe to my newsletter</p>
         </div>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit( async (data) => {
+            const newSub = {
+                firstName: data.firstName,
+                lastName: data.lastName,
+                email: data.email,
+            }
+            await fetch('http://localhost:5001/record/sub', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newSub)
+        })
+        .catch(error => {
+            window.alert(error);
+            return;
+        })
+        setValue('firstName', '')
+        setValue('lastName', '')
+        setValue('email', '')
+        }, onError)}>
 
         <div id="subscribeFormDiv" className='flex flex-col items-center px-5'>
-            <div id="firstName" className='w-full'>
+            <div id="firstName" className='w-full flex flex-col'>
                 <input 
                     type="text" 
-                    name='firstName' 
+                    {...register('firstName', {required: 'A first name is required'})}
                     placeholder='First Name' 
-                    className='w-full border border-[#676766] border-solid p-2 mb-2'
-                    value={firstName}
-                    onChange={e => setFirstName(e.target.value)}
+                    className='w-full border border-[#676766] border-solid p-2 mb-2 order-2'
                     />
+                <p>{errors.firstName?.message}</p>
             </div>
-            <div id="lastName" className='w-full'>
+            <div id="lastName" className='w-full flex flex-col'>
                 <input 
                     type="text" 
-                    name='lastName' 
+                    {...register('lastName', {required: 'A last name is required', minLength: {value: 2, message: 'Min length is 2'}})}
                     placeholder='Last Name' 
-                    className='w-full border border-[#676766] border-solid p-2 mb-2'
-                    value={lastName}
-                    onChange={e => setLastName(e.target.value)}
+                    className='w-full border border-[#676766] border-solid p-2 mb-2 order-2'
                     />
+                <p>{errors.lastName?.message}</p>
             </div>
-            <div id="email" className='w-full'>
+            <div id="email" className='w-full flex flex-col'>
                 <input 
                     type="email" 
-                    name="email" 
+                    {...register("email", {required: 'An email is required'})}
                     placeholder='email' 
-                    className='w-full border border-[#676766] border-solid p-2 mb-2'
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
+                    className='w-full border border-[#676766] border-solid p-2 mb-2 order-2'
                     />
+                <p>{errors.email?.message}</p>
             </div>
             <div id="subSubmit" className='w-full'>
                 <input id='submit' type="submit" name="submit"  className='w-full
