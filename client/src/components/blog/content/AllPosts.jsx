2d174cef17ab25bc676c1763ca838869ade2841c
roodhouse <rughjm@gmail.com>
2023-05-14 17:00:24 -0500

    removed 1st attempt at pagination. will try another

--- client/src/components/blog/content/AllPosts.jsx
+++ client/src/components/blog/content/AllPosts.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BlogList from './blogList'
 
 function AllPosts() {
+    
   return (
     <div name='posts' className='pt-[20rem] mx-auto px-8'>
         <BlogList />
