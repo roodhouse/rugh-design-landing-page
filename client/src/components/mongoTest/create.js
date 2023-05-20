677cd30a9d72ce0cd866db3ac11b3fb6a88458b4
roodhouse <rughjm@gmail.com>
2023-05-19 19:04:51 -0500

    updated fetch

--- client/src/components/mongoTest/create.js
+++ client/src/components/mongoTest/create.js
@@ -25,7 +25,7 @@ export default function Create() {
     // When a post request is sent to the create url, we'll add a new record to the database.
     const newPerson = { ...form };
 
-    await fetch("http://localhost:5001/record/add" || `https://rugh.design:5001/record/add` || `${process.env.URL}:/5001/record/add`, {
+    await fetch(`https://rugh.design:5001/record/add`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
