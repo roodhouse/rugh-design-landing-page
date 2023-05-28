00d79d7b1ac26c3ef07c173e359caf29564651c2
roodhouse <rughjm@gmail.com>
2023-05-27 19:39:29 -0500

    reg form

--- client/src/components/login/Register.jsx
+++ client/src/components/login/Register.jsx
@@ -1,57 +1,98 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function Register() {
-    const { register, handleSubmit, setValue, formState: {errors} } = useForm({defaultValues: {
-        firstName: '',
-        lastName: '',
-        email: ''
-    }});
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
 
-    const onError = () => {
-        console.log('wrong')
-    }
-    
+  const onError = () => {
+    console.log("wrong");
+  };
+
   return (
-    <div id='registerDiv' className='w-full p-2 pt-44 border border-solid border-[#676766] rounded-md xl:w-[75%]'>
-        <div id='regHeadingDiv' className='text-center'>
-            <h3 className='text-4xl text-[#676766] font-bold inline border-b-4 border-[#E5C1C1]'>Register</h3>
-        </div>
-        <div id='regTextDiv' className='text-center py-4'>
-            <p>let's go</p>
-        </div>
-        <form>
-        <div id="regFormDiv" className='flex flex-col items-center px-5'>
-            <div id="email" className='w-full flex flex-col'>
-                <input 
-                    type="email" 
-                    {...register('email', {required: 'An email is required'})}
-                    placeholder='email address' 
-                    className='w-full border border-[#676766] border-solid p-2 mb-2 order-2'
-                    />
-                <p>{errors.email?.message}</p>
-            </div>
-            <div id="password" className='w-full flex flex-col'>
-                <input 
-                    type="password" 
-                    {...register('password', {required: 'A password is required', minLength: {value: 2, message: 'Min length is 2'}})}
-                    placeholder='password' 
-                    className='w-full border border-[#676766] border-solid p-2 mb-2 order-2'
-                    />
-                <p>{errors.password?.message}</p>
-            </div>
-            <div id="confirmPassword" className='w-full flex flex-col'>
-                <input 
-                    type="password" 
-                    {...register('confirmPassword', {required: 'A password is required', minLength: {value: 2, message: 'Min length is 2'}})}
-                    placeholder='confirm your password' 
-                    className='w-full border border-[#676766] border-solid p-2 mb-2 order-2'
-                    />
-                <p>{errors.confirmPassword?.message}</p>
-            </div>
-            <div id="regSubmit" className='w-full'>
-                <input id='submit' type="submit" name="submit"  className='w-full
+    <div
+      id="registerDiv"
+      className="w-full p-2 pt-44 border border-solid border-[#676766] rounded-md xl:w-[75%]"
+    >
+      <div id="regHeadingDiv" className="text-center">
+        <h3 className="text-4xl text-[#676766] font-bold inline border-b-4 border-[#E5C1C1]">
+          Register
+        </h3>
+      </div>
+      <div id="regTextDiv" className="text-center py-4">
+        <p>let's go</p>
+      </div>
+      <form onSubmit={handleSubmit( async (data) => {
+        const newReg = {
+            email: data.email,
+            password: data.password
+        }
+
+        await fetch(`https://rugh.design:5001/record/reg`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newReg)
+        })
+        .catch(error => {
+            window.alert(error);
+            return;
+        })
+        setValue('email', '')
+        setValue('password', '')
+      }, onError)}>
+        <div id="regFormDiv" className="flex flex-col items-center px-5">
+          <div id="email" className="w-full flex flex-col">
+            <input
+              type="email"
+              {...register("email", { required: "An email is required" })}
+              placeholder="email address"
+              className="w-full border border-[#676766] border-solid p-2 mb-2 order-2"
+            />
+            <p>{errors.email?.message}</p>
+          </div>
+          <div id="password" className="w-full flex flex-col">
+            <input
+              type="password"
+              {...register("password", {
+                required: "A password is required",
+                minLength: { value: 2, message: "Min length is 2" },
+              })}
+              placeholder="password"
+              className="w-full border border-[#676766] border-solid p-2 mb-2 order-2"
+            />
+            <p>{errors.password?.message}</p>
+          </div>
+          <div id="confirmPassword" className="w-full flex flex-col">
+            <input
+              type="password"
+              {...register("confirmPassword", {
+                required: "A password is required",
+                minLength: { value: 2, message: "Min length is 2" },
+              })}
+              placeholder="confirm your password"
+              className="w-full border border-[#676766] border-solid p-2 mb-2 order-2"
+            />
+            <p>{errors.confirmPassword?.message}</p>
+          </div>
+          <div id="regSubmit" className="w-full">
+            <input
+              id="submit"
+              type="submit"
+              name="submit"
+              className="w-full
                                                                         cursor-pointer
                                                                         inline-block
                                                                         rounded 
@@ -61,15 +102,22 @@ function Register() {
                                                                         font-medium lowercase leading-normal text-[#676766] 
                                                                         shadow-[0_4px_9px_-4px_#cbcbcb] 
                                                                         transition duration-150 ease-in-out 
-                                                                        hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)]'
-                                                                        
-                                                               />
-            </div>
+                                                                        hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)]"
+            />
+          </div>
         </div>
-        </form>
-        <div className='p-4 text-sm text-center'><p>If you have already registered, you may login <Link className='text-[#e5c1c1] underline' to={'/login'}>here</Link>.</p></div>
+      </form>
+      <div className="p-4 text-sm text-center">
+        <p>
+          If you have already registered, you may login{" "}
+          <Link className="text-[#e5c1c1] underline" to={"/login"}>
+            here
+          </Link>
+          .
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
