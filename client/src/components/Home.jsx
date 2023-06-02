7b9ede28b7bc9b6bf27566e347cb94c8a0879d3e
roodhouse <rughjm@gmail.com>
2023-06-02 17:55:00 -0500

    playing with animejs package

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,22 @@ import {Link} from 'react-scroll'
 import Laura from '../assets/laura-round.png';
 import Navbar from './NavbarLanding';
 import { Helmet } from 'react-helmet-async';
-
+import anime from 'animejs/lib/anime.es.js';
+    
+// anime({
+//     targets:imgOfLaura,
+//     keyframes: [
+//         {translateY: -40},
+//         {translateX: 250},
+//         {translateY: 40},
+//         {translateX: 0},
+//         {translateY: 0}
+//       ],
+//       duration: 4000,
+//       easing: 'easeOutElastic(1, .8)',
+//       loop: true
+// }) 
+ 
 function Home() {
   return (
     <>
@@ -21,7 +36,12 @@ function Home() {
             <div className='flex flex-col sm:flex-row justify-center'>
                 <div>
                     <p className='text-[#676766]'>Hey Y'all! My name is</p>
-                    <h1 className='text-4xl sm:text-7xl font-bold text-[#E5C1C1]'>Laura Rugh</h1>
+                    <h1 className='text-4xl sm:text-7xl font-bold text-[#E5C1C1] lauraName'>
+                        <span id="textWrapper" className='text-wrapper'>
+                           <span className='letters'>Laura Rugh</span> 
+                            </span>
+                        
+                        </h1>
                     <h2 className='text-4xl sm:text-7xl font-bold text-[#676766]'>I'm a Color Consultant.</h2>
                     <p className='text-[#676766] py-4 max-w-[700px]'>I have a passion for design, color and helping people find the perfect paint for their spaces. Enjoy and let me know how I can help!</p>
                     <div>
@@ -35,7 +55,7 @@ function Home() {
                     </div>
                 </div>
                 <div className='ml-[5%] w-[100%] h-[100%] md:h-[75%] order-first sm:order-last pb-4 sm:pb-0'>
-                    <img className='rounded-full md:h-[100%]' src={Laura} alt="Laura Rugh" />
+                    <img id='imgOfLaura' className='rounded-full md:h-[100%]' src={Laura} alt="Laura Rugh" />
                 </div>
             </div>
         </div>
